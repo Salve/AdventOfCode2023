00ae96e2b692a2b3e4fe5367fb3c41e292af140d
Salve Spinnangr <salve@spinnangr.no>
2023-12-10 19:17:31 +0100

    Nasty overlapping matches...

--- day1/day1.go
+++ day1/day1.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/Salve/AdventOfCode2023/inputs"
 	"github.com/Salve/AdventOfCode2023/registry"
+	"strings"
 )
 
 const day = 1
@@ -21,11 +22,55 @@ func Run() {
 }
 
 func part1() {
-	result := "TODO"
+	sum := 0
+	for _, line := range inputs.Lines(input) {
+		sum += calibrationValue(line, digits)
+	}
+	result := sum
 	fmt.Printf("Part 1: %v\n", result)
 }
 
 func part2() {
-	result := "TODO"
+	sum := 0
+	for _, line := range inputs.Lines(input) {
+		sum += calibrationValue(line, alphaDigits)
+	}
+	result := sum
 	fmt.Printf("Part 2: %v\n", result)
+}
+
+func calibrationValue(line string, digits map[string]int) int {
+	combined := first(line, digits)*10 + last(line, digits)
+	return combined
+}
+
+func first(line string, digits map[string]int) int {
+	for pos := 0; pos < len(line); pos++ {
+		for k, v := range digits {
+			if strings.HasPrefix(line[pos:], k) {
+				return v
+			}
+		}
+	}
+	panic("eof")
+}
+
+func last(line string, digits map[string]int) int {
+	for pos := len(line); pos >= 0; pos-- {
+		for k, v := range digits {
+			if strings.HasSuffix(line[:pos], k) {
+				return v
+			}
+		}
+	}
+	panic("eof")
+}
+
+var digits = map[string]int{
+	"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
+}
+
+var alphaDigits = map[string]int{
+	"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
+	"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
 }
