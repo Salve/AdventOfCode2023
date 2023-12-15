de312dc676a334540305739efccb228526248d19
Salve Spinnangr <salve@spinnangr.no>
2023-12-15 16:46:50 +0100

    Day 5 - Naive p1 solution, blows up in p2.

--- day5/day5.go
+++ day5/day5.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"github.com/Salve/AdventOfCode2023/inputs"
 	"github.com/Salve/AdventOfCode2023/registry"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 const day = 5
@@ -21,18 +24,55 @@ func Run() {
 }
 
 func part1() {
-	result := "TODO"
+	lines := inputs.Lines(input)
+	var locations []int
+	for _, seed := range nums(strings.Split(lines[0], ": ")[1]) {
+		locations = append(locations, locationForSeed(seed, lines[1:]))
+	}
+	result := slices.Min(locations)
 	fmt.Printf("Part 1: %v\n", result)
 }
 
 func part2() {
-	result := "TODO"
+	lines := inputs.Lines(input)
+	var locations []int
+	seednums := nums(strings.Split(lines[0], ": ")[1])
+	for i := 0; i < len(seednums); i += 2 {
+		start, rnge := seednums[i], seednums[i+1]
+		for seed := start; seed < start+rnge; seed++ {
+			locations = append(locations, locationForSeed(seed, lines[1:]))
+		}
+	}
+	result := slices.Min(locations)
 	fmt.Printf("Part 2: %v\n", result)
+}
+
+func locationForSeed(seed int, almanac []string) int {
+	cur, next := seed, seed
+	for _, line := range almanac {
+		v := nums(line)
+		if len(v) < 3 { // We're at a new section, next is the new value to map
+			cur = next
+			continue
+		}
+		if newV := lookup(cur, v[0], v[1], v[2]); newV != 0 {
+			next = newV
+		}
+	}
+	return next
 }
 
 func lookup(value, dst, src, rnge int) int {
 	if value < src || value > src+rnge {
-		return value
+		return 0
 	}
+	return value + dst - src
+}
 
+func nums(line string) (o []int) {
+	for _, v := range strings.Fields(line) {
+		nv, _ := strconv.Atoi(v)
+		o = append(o, nv)
+	}
+	return o
 }
