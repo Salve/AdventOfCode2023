ff55cef10d4b7886ed1992c63da0a2db6025b706
Salve Spinnangr <salve@spinnangr.no>
2023-12-15 20:33:32 +0100

    Day 5 - Can't belive this mess worked on first compile...

--- day5/day5.go
+++ day5/day5.go
@@ -1,6 +1,7 @@
 package day5
 
 import (
+	"cmp"
 	"fmt"
 	"github.com/Salve/AdventOfCode2023/inputs"
 	"github.com/Salve/AdventOfCode2023/registry"
@@ -12,6 +13,7 @@ import (
 const day = 5
 
 var input []byte
+var a almanac
 
 func init() {
 	registry.Register(day, Run)
@@ -19,60 +21,149 @@ func init() {
 
 func Run() {
 	input = inputs.Input(day)
+	a = almanacFromInput(strings.TrimSuffix(string(input), "\n"))
 	part1()
 	part2()
 }
 
 func part1() {
-	lines := inputs.Lines(input)
-	var locations []int
-	for _, seed := range nums(strings.Split(lines[0], ": ")[1]) {
-		locations = append(locations, locationForSeed(seed, lines[1:]))
+	var locs []interval
+	for _, seed := range nums(strings.Split(inputs.Lines(input)[0], ": ")[1]) {
+		locs = append(locs, a.lookup(interval{seed, seed})...)
 	}
-	result := slices.Min(locations)
+
+	slices.SortFunc(locs, func(a, b interval) int {
+		return cmp.Compare(a.start, b.start)
+	})
+	result := locs[0].start
+
 	fmt.Printf("Part 1: %v\n", result)
 }
 
 func part2() {
-	lines := inputs.Lines(input)
-	var locations []int
-	seednums := nums(strings.Split(lines[0], ": ")[1])
-	for i := 0; i < len(seednums); i += 2 {
-		start, rnge := seednums[i], seednums[i+1]
-		for seed := start; seed < start+rnge; seed++ {
-			locations = append(locations, locationForSeed(seed, lines[1:]))
-		}
+	var locs []interval
+	for _, i := range seedIntervalsFromInput(input) {
+		locs = append(locs, a.lookup(i)...)
 	}
-	result := slices.Min(locations)
+	slices.SortFunc(locs, func(a, b interval) int {
+		return cmp.Compare(a.start, b.start)
+	})
+	result := locs[0].start
+
 	fmt.Printf("Part 2: %v\n", result)
 }
 
-func locationForSeed(seed int, almanac []string) int {
-	cur, next := seed, seed
-	for _, line := range almanac {
-		v := nums(line)
-		if len(v) < 3 { // We're at a new section, next is the new value to map
-			cur = next
-			continue
-		}
-		if newV := lookup(cur, v[0], v[1], v[2]); newV != 0 {
-			next = newV
-		}
+func seedIntervalsFromInput(input []byte) (o []interval) {
+	seednums := nums(strings.Split(inputs.Lines(input)[0], ": ")[1])
+	for i := 0; i < len(seednums); i += 2 {
+		start, rnge := seednums[i], seednums[i+1]
+		o = append(o, interval{
+			start: start,
+			end:   start + rnge - 1,
+		})
 	}
-	return next
+	return o
 }
 
-func lookup(value, dst, src, rnge int) int {
-	if value < src || value > src+rnge {
-		return 0
+func almanacFromInput(input string) (a almanac) {
+	for _, am := range strings.Split(input, "\n\n")[1:] {
+		a = append(a, almanacMapFromInput(am))
 	}
-	return value + dst - src
+	return a
+}
+
+func almanacMapFromInput(input string) (am almanacMap) {
+	for _, r := range strings.Split(input, "\n")[1:] {
+		am = append(am, ruleFromInput(r))
+	}
+	return am
+}
+
+func ruleFromInput(input string) rule {
+	vals := nums(input)
+	dst, src, rnge := vals[0], vals[1], vals[2]
+	return rule{
+		interval: interval{
+			start: src,
+			end:   src + rnge - 1,
+		},
+		offset: dst - src,
+	}
 }
 
 func nums(line string) (o []int) {
-	for _, v := range strings.Fields(line) {
-		nv, _ := strconv.Atoi(v)
-		o = append(o, nv)
+	for _, f := range strings.Fields(line) {
+		v, _ := strconv.Atoi(f)
+		o = append(o, v)
 	}
 	return o
+}
+
+type almanac []almanacMap
+
+type almanacMap []rule
+
+type rule struct {
+	interval
+	offset int
+}
+
+type interval struct {
+	start, end int
+}
+
+func (a almanac) lookup(i interval) []interval {
+	current := []interval{i}
+	for _, am := range a {
+		current = am.lookupMulti(current)
+	}
+	return current
+}
+
+func (am almanacMap) lookupMulti(intervals []interval) (o []interval) {
+	for _, i := range intervals {
+		o = append(o, am.lookup(i)...)
+	}
+	return o
+}
+
+func (am almanacMap) lookup(i interval) []interval {
+	mapped := []interval{}
+	unmapped := []interval{i}
+
+	for _, r := range am {
+		m, u := r.applyToMulti(unmapped)
+		mapped = append(mapped, m...)
+		unmapped = u
+	}
+	return append(mapped, unmapped...)
+}
+
+func (r rule) applyToMulti(intervals []interval) (mapped []interval, unmapped []interval) {
+	for _, i := range intervals {
+		m, u := r.applyTo(i)
+		mapped = append(mapped, m...)
+		unmapped = append(unmapped, u...)
+	}
+	return
+}
+
+func (r rule) applyTo(i interval) (mapped []interval, unmapped []interval) {
+	if i.end < r.start || i.start > r.end {
+		unmapped = []interval{i}
+		return
+	}
+	if i.start < r.start {
+		unmapped = append(unmapped, interval{i.start, r.start - 1})
+	}
+	common := interval{max(i.start, r.start), min(i.end, r.end)}
+	mapped = append(mapped, common.add(r.offset))
+	if i.end > r.end {
+		unmapped = append(unmapped, interval{r.end + 1, i.end})
+	}
+	return
+}
+
+func (i interval) add(v int) interval {
+	return interval{i.start + v, i.end + v}
 }
