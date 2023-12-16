ccfa4bb5c6d7e8092c7b4f4975e1f787309ea0a6
Salve Spinnangr <salve@spinnangr.no>
2023-12-16 13:42:50 +0100

    Day 7 - p2

--- day7/day7.go
+++ day7/day7.go
@@ -21,6 +21,8 @@ KTJJT 220
 QQQJA 483
 `)
 
+var jokersEnabled bool
+
 func init() {
 	registry.Register(day, Run)
 }
@@ -46,7 +48,17 @@ func part1() {
 }
 
 func part2() {
-	result := "TODO"
+	jokersEnabled = true
+	var entries []entry
+	for _, line := range inputs.Lines(input) {
+		entries = append(entries, entryFromLine(line))
+	}
+	winnings := 0
+	slices.SortFunc(entries, rankHands)
+	for i, e := range entries {
+		winnings += (i + 1) * e.bid
+	}
+	result := winnings
 	fmt.Printf("Part 2: %v\n", result)
 }
 
@@ -63,32 +75,45 @@ func rankType(a, b entry) int {
 
 func rankCards(a, b entry) int {
 	for i := 0; i < 5; i++ {
-		if c := cmp.Compare(a.hand[i], b.hand[i]); c != 0 {
-			return c * -1
+		av, bv := a.hand[i], b.hand[i]
+		if jokersEnabled && av == 11 {
+			av = 1
+		}
+		if jokersEnabled && bv == 11 {
+			bv = 1
+		}
+		if c := cmp.Compare(bv, av); c != 0 {
+			return c
 		}
 	}
+	fmt.Println("eq")
 	return 0
 }
 
 func (e entry) handType() int {
-	counts := make([]byte, 13)
+	counts := make([]byte, 15)
 	for _, v := range e.hand {
 		counts[v]++
+	}
+	var jokerCount byte
+	if jokersEnabled {
+		jokerCount = counts[11]
+		counts[11] = 0 // don't double count them
 	}
 	slices.Sort(counts)
 	slices.Reverse(counts)
 	switch {
-	case counts[0] == 5:
+	case counts[0]+jokerCount == 5:
 		return 1
-	case counts[0] == 4:
+	case counts[0]+jokerCount == 4:
 		return 2
-	case counts[0] == 3 && counts[1] == 2:
+	case counts[0]+jokerCount == 3 && counts[1] == 2:
 		return 3
-	case counts[0] == 3:
+	case counts[0]+jokerCount == 3:
 		return 4
-	case counts[0] == 2 && counts[1] == 2:
+	case counts[0] == 2 && counts[1]+jokerCount == 2:
 		return 5
-	case counts[0] == 2:
+	case counts[0]+jokerCount == 2:
 		return 6
 	default:
 		return 7
@@ -109,4 +134,4 @@ func entryFromLine(line string) (e entry) {
 	return e
 }
 
-var cardValue = map[rune]byte{'2': 0, '3': 1, '4': 2, '5': 3, '6': 4, '7': 5, '8': 6, '9': 7, 'T': 8, 'J': 9, 'Q': 10, 'K': 11, 'A': 12}
+var cardValue = map[rune]byte{'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
