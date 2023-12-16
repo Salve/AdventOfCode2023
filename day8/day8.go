fa9851f5adedf8e2db786ff1a4c84a1c10aa16c5
Salve Spinnangr <salve@spinnangr.no>
2023-12-16 15:36:55 +0100

    Day 8 - part 2

--- day8/day8.go
+++ day8/day8.go
@@ -23,24 +23,35 @@ func Run() {
 
 func part1() {
 	m := readMap(input)
-	result := m.stepsToZZZ(0, "AAA", inputs.Lines(input)[0])
+	result := m.stepsToZ(0, "AAA", inputs.Lines(input)[0])
 	fmt.Printf("Part 1: %v\n", result)
 }
 
 func part2() {
-	result := "TODO"
+	m := readMap(input)
+	directions := inputs.Lines(input)[0]
+	var cycles []int
+	for pos := range m {
+		if pos[2] == 'A' {
+			cycles = append(cycles, m.stepsToZ(0, pos, directions))
+		}
+	}
+	result := cycles[0]
+	for _, v := range cycles[1:] {
+		result = lcm(result, v)
+	}
 	fmt.Printf("Part 2: %v\n", result)
 }
 
-func (m camelMap) stepsToZZZ(stepsTaken int, position, directions string) int {
-	if position == "ZZZ" {
+func (m camelMap) stepsToZ(stepsTaken int, position, directions string) int {
+	if position[2] == 'Z' {
 		return stepsTaken
 	}
 	dir := 0
 	if directions[stepsTaken%len(directions)] == 'R' {
 		dir = 1
 	}
-	return m.stepsToZZZ(stepsTaken+1, m[position][dir], directions)
+	return m.stepsToZ(stepsTaken+1, m[position][dir], directions)
 }
 
 type camelMap map[string][2]string
@@ -52,4 +63,17 @@ func readMap(input []byte) camelMap {
 		o[n1] = [2]string{n2, n3}
 	}
 	return o
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
 }
